66d652ee3800a3c8523cde5640c6280005db0ca6
xiaokobegjw <249987839@qq.com>
2023-07-25 19:44:11 +0800

    小特效

---
+++ Resources/res/maps/scene_tree1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4385" name="scene_tree1" tilewidth="35" tileheight="36" tilecount="1" columns="1">
+ <image source="../../Resources/Textures/sceneObject/tree1/scene_tree1.png" width="35" height="36"/>
+</tileset>
